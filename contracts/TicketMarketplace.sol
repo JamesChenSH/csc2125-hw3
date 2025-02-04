4f77734dfe42d6378222f602d38634eecc4e1a48
shaohong chen <shaohong.chen@mail.utoronto.ca>
2025-02-04 12:33:06 -0500

    progress so far

--- contracts/TicketMarketplace.sol
+++ contracts/TicketMarketplace.sol
@@ -10,4 +10,99 @@ import "hardhat/console.sol";
 
 contract TicketMarketplace is ITicketMarketplace {
     // your code goes here (you can do it!)
+    address public ERC20Address;
+    address public nftContract;
+    address public owner;
+    
+    uint128 public currentEventId = 0;
+
+    constructor(address _sampleCoinAddress) {
+        ERC20Address = _sampleCoinAddress;
+        owner = msg.sender;
+    }
+
+    struct Event {
+        uint128 maxTickets;
+        uint128 nextTicketToSell;
+        uint256 pricePerTicket;
+        uint256 pricePerTicketERC20;
+    }
+
+    mapping(uint128 => Event) public events;
+
+    function createEvent(uint128 maxTickets, uint256 pricePerTicket, uint256 pricePerTicketERC20) public override {
+        if (msg.sender != owner) {
+            revert("Unauthorized access");
+        }
+        events[currentEventId] = Event(maxTickets, 0, pricePerTicket, pricePerTicketERC20);
+        emit EventCreated(currentEventId, maxTickets, pricePerTicket, pricePerTicketERC20);
+        currentEventId++;
+    }
+
+    function setMaxTicketsForEvent(uint128 eventId, uint128 newMaxTickets) public override{
+        if (owner != msg.sender) {
+            revert("Unauthorized access");
+        } else if (events[eventId].maxTickets >= newMaxTickets) {
+            revert("The new number of max tickets is too small!");
+        }
+        events[eventId].maxTickets = newMaxTickets;
+        emit MaxTicketsUpdate(eventId, newMaxTickets);
+    }
+
+    function setPriceForTicketETH(uint128 eventId, uint256 price) public override {
+        if (owner != msg.sender) {
+            revert("Unauthorized access");
+        }
+        events[eventId].pricePerTicket = price;
+        emit PriceUpdate(eventId, price, "ETH");
+    }
+
+    function setPriceForTicketERC20(uint128 eventId, uint256 price) public override{
+        if (owner != msg.sender) {
+            revert("Unauthorized access");
+        }
+        events[eventId].pricePerTicketERC20 = price;
+        emit PriceUpdate(eventId, price, "ERC20");
+    }
+
+    function buyTickets(uint128 eventId, uint128 ticketCount) payable public override{
+        
+        if (ticketCount > (2 ** 256 - 1) / events[eventId].pricePerTicket) {
+            revert("Overflow happened while calculating the total price of tickets. Try buying smaller number of tickets.");
+        } else if (events[eventId].nextTicketToSell + ticketCount > events[eventId].maxTickets) {
+            revert("We don't have that many tickets left to sell!");
+        } else if (msg.value < events[eventId].pricePerTicket * ticketCount) {
+            revert("Not enough funds supplied to buy the specified number of tickets.");
+        }
+
+        events[eventId].nextTicketToSell += ticketCount;
+        ITicketNFT(nftContract).mintFromMarketPlace(msg.sender, eventId);
+
+        emit TicketsBought(eventId, ticketCount, "ETH");
+    }
+
+    function buyTicketsERC20(uint128 eventId, uint128 ticketCount) public override{
+        if (ticketCount > (2 ** 256 - 1) / events[eventId].pricePerTicket) {
+            revert("Overflow happened while calculating the total price of tickets. Try buying smaller number of tickets.");
+        } else if (events[eventId].nextTicketToSell + ticketCount > events[eventId].maxTickets) {
+            revert("We don't have that many tickets left to sell!");
+        } else if (IERC20(ERC20Address).balanceOf(msg.sender) < events[eventId].pricePerTicketERC20 * ticketCount) {
+            revert("You don't have enough ERC20 tokens to buy these tickets!");
+        }
+
+        events[eventId].nextTicketToSell += ticketCount;
+        IERC20(ERC20Address).transferFrom(msg.sender, owner, events[eventId].pricePerTicketERC20 * ticketCount);
+        ITicketNFT(nftContract).mintFromMarketPlace(msg.sender, eventId);
+
+        emit TicketsBought(eventId, ticketCount, "ERC20");
+
+    }
+
+    function setERC20Address(address newERC20Address) public override{
+        if (owner != msg.sender) {
+            revert("Unauthorized access");
+        }
+        ERC20Address = newERC20Address;
+        emit ERC20AddressUpdate(newERC20Address);
+    }
 }
--- contracts/TicketNFT.sol
+++ contracts/TicketNFT.sol
@@ -5,8 +5,20 @@ import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {ITicketNFT} from "./interfaces/ITicketNFT.sol";
 
 // Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 
 contract TicketNFT is ERC1155, ITicketNFT {
     // your code goes here (you can do it!)
+    uint256 private ID = 0;
+    address public owner;
+
+    constructor (uint256 eventID, uint256 seatID) ERC1155("") {
+        ID = (eventID << 128) | seatID;
+        owner = msg.sender;
+        _mint(msg.sender, ID, 1, "");
+    }
+
+    function mintFromMarketPlace(address to, uint256 nftId) public {
+        _mint(to, nftId, 1, "");
+    }
 }
